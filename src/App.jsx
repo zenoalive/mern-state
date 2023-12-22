6cab09d665cd49cb450e8c3d1af858016cc384bd
Zenoalive <ashushahi22229@gmail.com>
2023-12-22 14:29:46 +0530

    create pages and route

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Profile from './pages/Profile'
+import SignUp from './pages/SignUp'
+import SignIn from './pages/SignIn'
 
 export default function App() {
   return (
-    <h1 className='text-red-500'>
-      React App Mern
-    </h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element ={<Home />} />
+        <Route path='/About' element ={<About />} />
+        <Route path='/Profile' element ={<Profile />} />
+        <Route path='/sign-in' element ={<SignIn />} />
+        <Route path='/sign-up' element ={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
