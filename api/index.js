93597bbc98372fc4c392b74c83b1c13a4190cdc8
Zenoalive <ashushahi22229@gmail.com>
2024-01-04 13:38:35 +0530

    create a test api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
+import router from './routes/user-route.js'
+// import {test} from './controllers/user-controller.js'
 
 dotenv.config()
 
@@ -19,3 +21,10 @@ const app =express()
 app.listen(3000, () => {
     console.log('server is running on port 3000!! wow')
 })
+
+// req is the data sent by client(user), res is the sent by the server
+// app.get('/test', (req, res) => {
+//     res.send('Test AP2I')
+// })
+
+app.use("/api/user", router)
