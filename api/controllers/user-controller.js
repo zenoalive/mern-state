c1cf9f94d6d8df93b439f436dd6409cbc5127c49
Zenoalive <ashushahi22229@gmail.com>
2024-01-29 20:17:18 +0530

    Create user update API

--- api/controllers/user-controller.js
+++ api/controllers/user-controller.js
@@ -1,5 +1,44 @@
+import User from "../models/user-model.js"
+import { errorHandler } from "../utils/error.js"
+import bcryptjs from 'bcryptjs'
+
 export const test = (req, res) => {
     res.json({
         message : "API is working"
     })
 }
+
+// 
+
+export const updateUser = async (req, res, next) => {
+    if (req.user.id !== req.params.id) {
+        // Assuming errorHandler is a function that sends an error response
+        return next(errorHandler(401, 'You are not authenticated'));
+    }
+
+    try {
+        if (req.body.password) {
+            req.body.password = bcryptjs.hashSync(req.body.password, 10);
+        }
+
+        const updatedUser = await User.findByIdAndUpdate(
+            req.params.id,
+            {
+                $set: {
+                    username: req.body.username,
+                    email: req.body.email,
+                    password: req.body.password,
+                    avatar: req.body.avatar
+                }
+            },
+            { new: true }
+        );
+
+        // Fix the variable name here
+        const { password, ...rest } = updatedUser._doc;
+
+        res.status(200).json(rest);
+    } catch (error) {
+        next(error);
+    }
+};
--- api/index.js
+++ api/index.js
@@ -2,7 +2,8 @@ import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import userRouter from './routes/user-route.js'
-import authRouter from './routes/auth-route.js' 
+import authRouter from './routes/auth-route.js'
+import cookieParser from 'cookie-parser'
 // import {test} from './controllers/user-controller.js'
 
 dotenv.config()
@@ -20,6 +21,7 @@ mongoose.connect(process.env.MONGO).then(() => {
 const app =express()
 
 app.use(express.json())
+app.use(cookieParser())
 
 app.listen(3000, () => {
     console.log('server is running on port 3000!! wow')
