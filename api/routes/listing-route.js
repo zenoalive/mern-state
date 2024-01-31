ccea626084755ba347c9f1ebd86cc31ecba5872d
Zenoalive <ashushahi22229@gmail.com>
2024-01-31 13:22:13 +0530

    create listing api route

---
+++ api/routes/listing-route.js
@@ -0,0 +1,10 @@
+import express from 'express'
+import { createListing } from '../controllers/listing-controller.js'
+import { verifyToken } from '../utils/verifyUser.js'
+
+const router = express.Router()
+
+// Check authentication before creating a listing
+router.post('/create',verifyToken, createListing)
+
+export default router
