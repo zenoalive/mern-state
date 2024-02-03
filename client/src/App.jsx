1fdca3e8e6420ed45929eba8638cb2d24b542d5d
Zenoalive <ashushahi22229@gmail.com>
2024-02-03 19:53:45 +0530

    Add image slider to the listing page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,7 @@ import Header from './components/Header'
 import PrivateRout from './components/PrivateRout'
 import CreateListing from './pages/CreateListing'
 import UpdateListing from './pages/UpdateListing'
+import Listing from './pages/Listing'
 
 
 export default function App() {
@@ -18,14 +19,16 @@ export default function App() {
       <Routes>
         <Route path='/' element ={<Home />} />
         <Route path='/About' element ={<About />} />
+        <Route path='/sign-in' element ={<SignIn />} />
+        <Route path='/sign-up' element ={<SignUp />} />
+        <Route path='/listing/:listingId' element ={<Listing />} />
         <Route element={<PrivateRout />}>
           <Route path='/Profile' element ={<Profile />} />
           <Route path='/create-listing' element ={<CreateListing />} />
           <Route path='/update-listing/:listingId' element ={<UpdateListing />} />
         </Route>
        
-        <Route path='/sign-in' element ={<SignIn />} />
-        <Route path='/sign-up' element ={<SignUp />} />
+       
       </Routes>
     </BrowserRouter>
   )
---
+++ client/src/pages/Listing.jsx
@@ -0,0 +1,61 @@
+import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage'
+import React, { useEffect } from 'react'
+import { useState } from 'react'
+import { app } from '../firebase.js'
+import { useSelector } from 'react-redux'
+import { useActionData, useNavigate, useParams } from 'react-router-dom'
+import {Swiper, SwiperSlide} from 'swiper/react'
+import SwiperCore from 'swiper'
+import {Navigation} from 'swiper/modules'
+import 'swiper/css/bundle'
+
+export default function Listing() {
+    SwiperCore.use([Navigation])
+    const params = useParams()
+    const [listing, setListing] = useState(null)
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState(false)
+    useEffect(() => {
+        const fetchListing = async () => {
+            try {
+                setLoading(true)
+                const listingId = params.listingId
+                const res  = await fetch(`/api/listing/get/${listingId}`);
+                const data = await res.json()
+                if (data.success == false) {
+                    setError(true)
+                    setLoading(false)
+                }
+                setListing(data);
+                setLoading(false);
+                setError(false)
+
+            } catch (error) {
+                setError(true)
+            }
+        }
+        
+       
+    
+        fetchListing()
+    }, [params.listingId])
+    return (
+        <main>
+            {loading && <p className=' text-centermy-7 text-2xl'>loading...</p>}
+            {error && <p className=' text-centermy-7 text-2xl'>{"Something went wrongg"}</p> }
+            {listing && !loading && (
+                <>
+                    <Swiper naviagtion>
+                        {listing.imageURLs.map((url) => {
+                            <SwiperSlide key={url}>
+                                <div className='h-[550px]' style={{background: `url(${url}) centet no-repeat` }}>
+
+                                </div>
+                            </SwiperSlide>
+                        })}
+                    </Swiper>
+                </>
+            )}
+        </main>
+    )
+}
