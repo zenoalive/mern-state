a341968d462158d239b77afd75679d9798dd1624
Zenoalive <ashushahi22229@gmail.com>
2024-02-08 20:23:23 +0530

    Add Show More listings functionality

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -15,7 +15,7 @@ export default function Search() {
     });
     const [loading, setLoading] = useState(false)
     const [listings, setListings] = useState([])
-    console.log(listings)
+    const [showMore, setShowMore] = useState(false)
     
     useEffect(() => {
         const urlParams = new URLSearchParams(location.search)
@@ -49,9 +49,16 @@ export default function Search() {
 
         const fetchListings = async () => {
             setLoading(true)
+            setShowMore(false)
             const searchQuery = urlParams.toString()
             const res = await fetch(`/api/listing/get?${searchQuery}`);
             const data = await res.json()
+            if(data.length > 8) {
+                setShowMore(true)
+            }
+            else {
+                setShowMore(false)
+            }
             setListings(data)
             setLoading(false)
         }
@@ -78,6 +85,19 @@ export default function Search() {
         }
     }
 
+    const onShowMoreClick = async (e) => {
+        const numberOfListings = listings.length
+        const startIndex = numberOfListings - 9
+        urlParams.set('startIndex', startIndex)
+        const searchQuery = urlParams.toString()
+        const res = await fetch(`/api/listing/get?${searchQuery}`)
+        const data = res.json()
+        if(data.length < 9) {
+            setShowMore(false)
+        }
+
+        setListings([...listings, ...data])
+    }
     const handleSubmit = (e) => {
         e.preventDefault();
         const urlParams = new URLSearchParams()
@@ -173,9 +193,14 @@ export default function Search() {
                         listings.map((listing) => 
                          (<ListingItem key = {listing._id} listing={listing}/>) 
                         )
-                    )}                                                                                                                                                                                                                                 
+                    )}
+                    {showMore && (
+                        <button onClick={() => onShowMoreClick()} className='text-green-700 hover:underline text-center w-full'>
+                            Show More
+                        </button>
+                    )}                                                                                                                                                                                                                            
                 </div>
             </div>
         </div>
     )
-}
+} 
