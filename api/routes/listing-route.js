9a02a7d14568851682b40122a45bc5e6a651ef1d
Zenoalive <ashushahi22229@gmail.com>
2024-03-12 13:55:15 +0530

    Working

--- api/routes/listing-route.js
+++ api/routes/listing-route.js
@@ -5,7 +5,7 @@ import { verifyToken } from '../utils/verifyUser.js'
 const router = express.Router()
 
 // Check authentication before creating a listing
-router.post('/create/:id', verifyToken, createListing)
+router.post('/create/', verifyToken, createListing)
 router.delete('/delete/:id', verifyToken, deleteListing)
 router.post('/update/:id', verifyToken, updateListing)
 router.get('/get/:id', getListing)
